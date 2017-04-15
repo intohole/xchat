2359b6d19e3a34b036d1554f019e78a2fede3725
intohole <intoblack86@gmail.com>
2017-04-15 23:51:10 +0800

    add code

---
+++ xchat/main.py
@@ -0,0 +1,19 @@
+#coding-utf-8
+
+
+
+
+import itchat
+
+itchat.auto_login(enableCmdQR=True)
+
+
+from collections import defaultdict
+from b2 import consle2
+
+
+_chats = defaultdict(list)
+
+@itchat.msg_register(TEXT)
+def simple_reply(msg):
+    print msg 
